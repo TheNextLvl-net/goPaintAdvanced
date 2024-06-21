5b50731ca9fb80028f7cecf039bb3712af7fbf20
david <mrminecraft00@gmail.com>
2024-06-21 21:16:27 +0200

    localize mask mode

--- api/src/main/java/net/thenextlvl/gopaint/api/model/MaskMode.java
+++ api/src/main/java/net/thenextlvl/gopaint/api/model/MaskMode.java
@@ -3,38 +3,32 @@ package net.thenextlvl.gopaint.api.model;
 import com.sk89q.worldedit.EditSession;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+import net.kyori.adventure.translation.Translatable;
 import net.thenextlvl.gopaint.api.brush.Brush;
 import net.thenextlvl.gopaint.api.brush.setting.BrushSettings;
 import org.bukkit.block.Block;
 
-import java.util.Arrays;
-import java.util.Optional;
-
 @Getter
 @RequiredArgsConstructor
-public enum MaskMode {
+@Accessors(fluent = true)
+public enum MaskMode implements Translatable {
     /**
      * This enumeration represents that no mask should be applied.
      */
-    DISABLED("Disabled"),
+    DISABLED("mask.mode.disabled"),
     /**
      * This enumeration represents that the mask material defined in the interface should be applied
      *
      * @see Brush#passesMaskCheck(BrushSettings, EditSession, Block)
      */
-    INTERFACE("Interface"),
+    INTERFACE("mask.mode.interface"),
     /**
      * This enumeration represents the complex mask defined by WorldEdit
      *
      * @see Brush#passesMaskCheck(BrushSettings, EditSession, Block)
      */
-    WORLDEDIT("WorldEdit");
+    WORLDEDIT("mask.mode.worldedit");
 
-    private final String name;
-
-    public static Optional<MaskMode> byName(String name) {
-        return Arrays.stream(values())
-                .filter(maskMode -> maskMode.getName().equals(name))
-                .findAny();
-    }
+    private final String translationKey;
 }
