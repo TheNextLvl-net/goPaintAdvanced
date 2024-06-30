2ef424bc52c7cdb87da7a1a3b97bd98316243f2d
david <mrminecraft00@gmail.com>
2024-06-30 21:48:01 +0200

    renamed surface modes for clarity

--- api/src/main/java/net/thenextlvl/gopaint/api/model/SurfaceMode.java
+++ api/src/main/java/net/thenextlvl/gopaint/api/model/SurfaceMode.java
@@ -1,35 +1,32 @@
 package net.thenextlvl.gopaint.api.model;
 
+import com.fastasyncworldedit.core.function.mask.SurfaceMask;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 import net.kyori.adventure.translation.Translatable;
-import net.thenextlvl.gopaint.api.math.Surface;
-import org.bukkit.Location;
-import org.bukkit.block.Block;
+import net.thenextlvl.gopaint.api.brush.mask.VisibleMask;
 
 @Getter
 @RequiredArgsConstructor
 @Accessors(fluent = true)
 public enum SurfaceMode implements Translatable {
     /**
-     * This enumeration represents a more intuitive check.
-     *
-     * @see Surface#isDirectlyOnSurface(Block)
-     */
-    DIRECT("surface.mode.direct"),
-    /**
      * This enumeration represents that surface mode is disabled.
-     *
-     * @see Surface#isOnSurface(Block, SurfaceMode, Location)
      */
     DISABLED("surface.mode.disabled"),
     /**
+     * This enumeration represents a more intuitive check.
+     *
+     * @see SurfaceMask
+     */
+    EXPOSED("surface.mode.exposed"),
+    /**
      * This enumeration represents the original surface mode check.
      *
-     * @see Surface#isRelativelyOnSurface(Block, Location)
+     * @see VisibleMask
      */
-    RELATIVE("surface.mode.relative");
+    VISIBLE("surface.mode.visible");
 
     private final String translationKey;
 }
