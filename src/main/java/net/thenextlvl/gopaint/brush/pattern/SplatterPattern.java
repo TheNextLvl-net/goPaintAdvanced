134c41704e2fdf1d6c7ec918a85ae88fa055f637
david <mrminecraft00@gmail.com>
2024-07-02 19:29:37 +0200

    fixed splatter pattern rate calculation

--- src/main/java/net/thenextlvl/gopaint/brush/pattern/SplatterPattern.java
+++ src/main/java/net/thenextlvl/gopaint/brush/pattern/SplatterPattern.java
@@ -22,8 +22,8 @@ public record SplatterPattern(
     }
 
     private double getRate(BlockVector3 position) {
-        var size = (double) settings().getBrushSize();
-        var falloff = (100.0 - (double) settings().getFalloffStrength()) / 100.0;
-        return (position.distance(position()) - size * falloff) / (size - size * falloff);
+        var falloff = (100 - settings().getFalloffStrength()) / 100d;
+        return (position.distance(position()) - settings().getBrushSize() * falloff)
+               / (settings().getBrushSize() - settings().getBrushSize() * falloff);
     }
 }
