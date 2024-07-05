6f4b07b0d0cf7a5be71bf3c05d48b4c18a5d126d
david <mrminecraft00@gmail.com>
2024-07-05 22:35:32 +0200

    [ci skip] 1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ repositories {
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.32")
     compileOnly("net.thenextlvl.core:annotations:2.0.1")
-    compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
 
     implementation("org.bstats:bstats-bukkit:3.0.2")
 
@@ -40,7 +40,7 @@ paper {
     name = "goPaintAdvanced"
     main = "net.thenextlvl.gopaint.GoPaintPlugin"
     authors = listOf("Arcaniax", "TheMeinerLP", "NonSwag")
-    apiVersion = "1.20"
+    apiVersion = "1.21"
     foliaSupported = true
 
     serverDependencies {
